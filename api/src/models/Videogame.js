a133decd1c8e7e9b092818f2df1420b5527d4401
lucaaramos <lucaramos99@gmail.com>
2022-12-10 18:14:43 -0300

    DB Models

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,38 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    released: {
+      type: DataTypes.STRING
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+    },
+    platform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    img: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createDB: {  //sirve para distinguir entre videojuegos de la api y bd
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      allowNull: false
+    }
+
+  }, {timestamps: false});
 };
